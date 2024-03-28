7af3bc06153786dc11ee241f4872fb48ac5aa73d
Aman Kanojiya <amanskanojiya@gmail.com>
2024-03-28 07:31:08 +0530

    commit

---
+++ Demo.java
@@ -0,0 +1,7 @@
+import java.util.Scanner;
+public class Demo {
+    public static void main(String[] args) {
+       System.out.println("args 1 is: "+ args[0]);
+       System.out.println("args 2 is: "+ args[1]);
+}
+}
